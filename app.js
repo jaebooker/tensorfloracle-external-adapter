43dbdaaf68ba99b7d9380864a36a847287d854c2
Jaeson Booker <jaesonbooker@gmail.com>
2020-06-09 13:24:37 -0700

    adding infura get request

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const createRequest = require('./index').createRequest
-
+var request = new XMLHttpRequest()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -22,6 +22,13 @@ py.stdin.write(JSON.stringify(data));
 py.stdin.end();
 
 app.post('/', (req, res) => {
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', 'https://ipfs.infura.io:5001/api/v0/object/stat?arg=hash', true)
+  request.onload = function() {
+    // Begin accessing JSON data here
+  }
+  // Send request
+  request.send()
   console.log('POST Data: ', req.body)
   createRequest(req.body, (status, result) => {
     console.log('Result: ', result)
