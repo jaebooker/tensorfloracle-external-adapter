2dcca8a1d0ee23f8b7fffa2e44e7d39671502c1c
Jaeson Booker <jaesonbooker@gmail.com>
2020-06-08 20:09:01 -0700

    adding infura and kares

--- keras.py
+++ keras.py
@@ -2,12 +2,16 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#x_test, y_test = curl "https://ipfs.infura.io:5001/api/v0/get?arg=QmZtmD2qt6fJot32nabSP3CUjicnypEBz7bHVDhPQt9aAy&archive=true", curl "https://ipfs.infura.io:5001/api/v0/get?arg=QmZtmD2qt6fJot32nabSP3CUjicnypEBz7bHVDhPQt9aAy&archive=true"
+model = []
+results = []
+
+#x_test, y_test = curl "https://ipfs.infura.io:5001/api/v0/get?arg=ipfs_test_x&archive=true", curl "https://ipfs.infura.io:5001/api/v0/get?arg=ipfs_test_y&archive=true"
 
 test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 test_dataset = test_dataset.batch(64)
 
-#model = 'curl "https://ipfs.infura.io:5001/api/v0/get?arg=QmZtmD2qt6fJot32nabSP3CUjicnypEBz7bHVDhPQt9aAy&archive=true"'
+#for i in range(0, len(ipfs_train_hash)):
+#   model[i] = 'curl "https://ipfs.infura.io:5001/api/v0/get?arg=ipfs_train_hash[i]&archive=true"'
+#   results[i] = model[i].evaluate(test_dataset)
 
-result = model.evaluate(test_dataset)
-dict(zip(model.metrics_names, result))
+dict(zip(model.metrics_names, results))
