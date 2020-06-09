236ee35f5c3c303e58fb45fd99bf25b4ab30aef2
Jaeson Booker <jaesonbooker@gmail.com>
2020-06-09 13:14:23 -0700

    creating python spawn

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const port = process.env.EA_PORT || 8080
 
 app.use(bodyParser.json())
 
+var spawn = require('child_process').spawn,
+  py    = spawn('python', ['keras.py']),
+  data = [1,2,3,4,5,6,7,8,9],
+  dataString = '';
+
 app.post('/', (req, res) => {
   console.log('POST Data: ', req.body)
   createRequest(req.body, (status, result) => {
