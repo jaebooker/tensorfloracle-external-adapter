82c6ffc2648351a1e59169fd8401b1ebdeba441a
Jaeson Booker <jaesonbooker@gmail.com>
2020-06-09 13:19:29 -0700

    more pythonic integration

--- keras.py
+++ keras.py
@@ -1,3 +1,4 @@
+import sys, json, numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -15,3 +16,26 @@ test_dataset = test_dataset.batch(64)
 #   results[i] = model[i].evaluate(test_dataset)
 
 dict(zip(model.metrics_names, results))
+
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+
+    #create a numpy array
+    np_lines = np.array(lines)
+
+    #use numpys sum method to find sum of all elements in the array
+    lines_sum = np.sum(np_lines)
+
+    #return the sum to the output stream
+    print lines_sum
+
+#start process
+if __name__ == '__main__':
+    main()
